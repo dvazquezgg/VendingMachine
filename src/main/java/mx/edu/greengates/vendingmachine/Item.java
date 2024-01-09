95061d9c3c7f8559be4c0769cce547fea0005c8b
dvazquezgg <35460585+dvazquezgg@users.noreply.github.com>
2024-01-09 11:20:46 -0600

    New Methods to fill the Vending Machine

--- src/main/java/mx/edu/greengates/vendingmachine/Item.java
+++ src/main/java/mx/edu/greengates/vendingmachine/Item.java
@@ -1,2 +1,35 @@
-package mx.edu.greengates.vendingmachine;public class Item {
+package mx.edu.greengates.vendingmachine;
+
+public class Item {
+    public int id;
+    public String name;
+    public double price;
+    public int quantity;
+
+    public Item(int id, String name, double price, int quantity) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+    
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public String toString() {
+        return "Item: " + id + " " + name + " " + price + " " + quantity;
+    }
 }
